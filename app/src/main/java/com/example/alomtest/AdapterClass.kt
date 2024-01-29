75d4414590bb53c3d8342f74e7bfb1ed12cdea56
MisterJerry123 <misterjerry@sju.ac.kr>
2024-01-30 03:20:25 +0900

    //에러코드이므로 사용 하지 말것

---
+++ app/src/main/java/com/example/alomtest/AdapterClass.kt
@@ -0,0 +1,106 @@
+package com.example.alomtest
+
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alomtest.DataClass
+import com.example.alomtest.R
+
+
+class AdapterClass (private val dataList:ArrayList<DataClass>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    val ITEM =0
+    val FOOTER=1
+    interface OnItemClickListener{
+        fun onItemClick(position:Int)
+    }
+    private var itemClickListener: OnItemClickListener?=null
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType){
+            ITEM ->{val itemView=LayoutInflater.from(parent.context).inflate(
+                R.layout.item_layout,
+                parent, false)
+                ViewHolderClass(itemView)
+            }
+            FOOTER ->{val view = LayoutInflater.from(parent.context).inflate(R.layout.itemfooter_layout, parent, false)
+                FooterViewHolderClass(view, parent.context)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+
+            }
+        }
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        this.itemClickListener = listener
+    }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(getItemViewType(position)){
+            ITEM->{
+                val itemViewHolder = holder as ViewHolderClass
+
+               itemViewHolder.bind(dataList[position])
+               itemViewHolder.itemView.setOnClickListener {
+                  itemClickListener?.onItemClick(position)
+                   val context = it.context
+                   val dataTitle = dataList[position].dataTitle
+
+
+                   // 커스텀 다이얼로그 표시
+                   val customDialog = Food_dialog(context, dataTitle)
+                   customDialog.show()
+               }
+
+
+            }
+            FOOTER->{val footerViewHolder = holder as FooterViewHolderClass
+            footerViewHolder.bindFooterData()
+                footerViewHolder.itemView.setOnClickListener {
+                    itemClickListener?.onItemClick(dataList.size)
+                }}
+
+
+
+    }
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size+1
+    }
+    override fun getItemViewType(position: Int): Int {
+        return if (position < dataList.size) ITEM else FOOTER
+    }
+    inner class FooterViewHolderClass(itemView: View, private val context: Context) : RecyclerView.ViewHolder(itemView) {
+        private val footerImageView:ImageView
+        init {
+            val footerView = LayoutInflater.from(context).inflate(R.layout.itemfooter_layout, null)
+            // 클릭 이벤트 처리
+            footerImageView = footerView.findViewById((R.id.footerImageView))
+            itemView.setOnClickListener {
+                // Footer를 클릭했을 때 다른 화면으로 이동하는 코드
+                val intent = Intent(context, AddActivity::class.java)
+
+                context.startActivity(intent)
+            }
+        }
+        fun bindFooterData(){
+            footerImageView.setImageResource(R.drawable.food_item)
+        }
+    }
+
+
+
+    class ViewHolderClass(itemView: View):RecyclerView.ViewHolder(itemView) {
+        private val rvTitle: TextView = itemView.findViewById(R.id.food_text)
+        fun bind(data: DataClass){
+            rvTitle.text = data.dataTitle
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/alomtest/FoodAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.alomtest
+
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alomtest.retrofit.food_list
+
+class FoodAdapter(private var mList: MutableList<food_list>) :
+    RecyclerView.Adapter<FoodAdapter.FoodViewHolder>(){
+    private var onItemClickListener: ((String) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (String) -> Unit) {
+        onItemClickListener = listener
+    }
+
+
+    inner class FoodViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val titleTv : TextView = itemView.findViewById(R.id.titleTv)
+        init {
+            // 아이템 뷰가 클릭되었을 때의 동작을 정의
+            itemView.setOnClickListener {
+                onItemClickListener?.invoke(mList[absoluteAdapterPosition].name)
+            }
+    }}
+
+//    fun setFilteredList(mList: List<FoodData>){
+//        this.mList = mList.toMutableList()
+//        notifyDataSetChanged()
+//    }
+fun setFilteredList(mList: List<food_list>) {
+//    mList.clear()
+//    mList.addAll(filteredData)
+    this.mList = mList.toMutableList()
+    notifyDataSetChanged()
+}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
+        return FoodViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        holder.titleTv.text = mList[position].name
+
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+
+    //삭제
+    fun deleteItem(i : Int){
+        mList.removeAt(i)
+        notifyDataSetChanged()
+    }
+
+//    fun saveData(context: Context) {
+//        val preferences = context.getSharedPreferences("food_data", Context.MODE_PRIVATE)
+//        val editor = preferences.edit()
+//        val jsonStringList = mList.map { it.toJsonString()}
+//        editor.putStringSet("food_list", jsonStringList.toSet())
+//        editor.apply()
+//    }
+//
+//    fun loadData(context: Context) {
+//        val preferences = context.getSharedPreferences("food_data", Context.MODE_PRIVATE)
+//        val jsonStringList = preferences.getStringSet("food_list", emptySet())
+//        mList = jsonStringList?.map { FoodData.fromString(it) }?.toMutableList() ?: mutableListOf()
+//        notifyDataSetChanged()
+//    }
+
+}
